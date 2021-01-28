19da7fe3b4095349715d375f420cfe34fcce7108
ELA Innovation <be-dev@Mac-mini-de-ELA.local>
2021-01-28 12:11:19 +0100

    ajout des capteur dans la factory

--- application/ios-blue-tool-box/View/BlueMAGViewController.swift
+++ application/ios-blue-tool-box/View/BlueMAGViewController.swift
@@ -8,21 +8,46 @@
 import UIKit
 import CoreBluetooth
 import SwiftUI
+
 class BlueMAGViewController: UIViewController {
     
 
-    
+    private let tableview : UITableView =
+    {
+        let tableview = UITableView()
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableview
+        
+    }()
        
     private var scanner: Scanner1!
     
+    
+    
+    
+/*
 
+    
+    
     private let progressView: UIProgressView =
     {
         let progressView = UIProgressView(progressViewStyle: .bar)
         progressView.trackTintColor = .gray
         progressView.progressTintColor = .systemBlue
+           
         return progressView
     }()
+    
+    private let progressView1: UILabel =
+    {
+        let progressView1 = UILabel()
+        progressView1.text = "ok"
+        
+        
+        return progressView1
+    
+    }()
+        
     
     
     struct ContentView: View {
@@ -30,6 +55,9 @@ class BlueMAGViewController: UIViewController {
             Text("Hello World")
         }
     }
+    */
+ 
+    
     
     override func viewDidLoad()
     {
@@ -38,6 +66,10 @@ class BlueMAGViewController: UIViewController {
         // Do any additional setup after loading the view.
         scanner = Scanner1()
         scanner.initializeScanner()
+        tableview.backgroundColor = .link
+        
+        view.addSubview(tableview)
+        
       
        /*let dic =  SensorFactory.shared().returnDtictionnary()
         for key in dic
@@ -46,19 +78,28 @@ class BlueMAGViewController: UIViewController {
         }
         */
         
-        view.backgroundColor = .darkGray
-        
+      /* view.backgroundColor = .darkGray
+
         
             view.addSubview(progressView)
+     let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+     label.center = CGPoint(x: 160, y: 285)
+     label.textAlignment = .center
+     label.text = "I'm a test label"
+     self.view.addSubview(label)
                 progressView.frame = CGRect(x: 10, y: 100, width: view.frame.width-20, height: 20)
                 progressView.setProgress(0.5, animated: true)
             
-           
+        
+        */
        
      
         
     }
-    
+    override func  viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableview.frame = view.bounds
+    }
     
     
       
