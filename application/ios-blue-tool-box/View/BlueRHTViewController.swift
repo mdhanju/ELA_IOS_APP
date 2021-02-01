6f52ba65d588a4ca76e6ac1a86f8a0c0c5adaf55
ELA Innovation <be-dev@Mac-mini-de-ELA.local>
2021-02-01 15:18:04 +0100

    ajout des capteur dans la factory

--- application/ios-blue-tool-box/View/BlueRHTViewController.swift
+++ application/ios-blue-tool-box/View/BlueRHTViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import Charts
+class BlueRHTViewController: UIViewController,ChartViewDelegate {
 
-class BlueRHTViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-view.backgroundColor = .white
-        // Do any additional setup after loading the view.
-    }
+    var lineChart = LineChartView()
+      
+      
+      
+      
+      
+      
+      
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          view.backgroundColor = .cyan
+          lineChart.delegate = self
+          
+      }
+      
+      override func viewDidLayoutSubviews() {
+          super.viewDidLayoutSubviews()
+          
+          
+            lineChart.frame = CGRect(x: 0, y: 0,
+                                       width: self.view.frame.size.width, height:self.view.frame.size.width)
+            lineChart.center = view.center
+            view.addSubview(lineChart)
+            
+            var entries = [ChartDataEntry]()
+            
+            for x in 0..<10
+            {
+                entries.append(ChartDataEntry(x: Double(x),y: Double(x)))
+            }
+            
+            let set = LineChartDataSet(entries:entries)
+            set.colors = ChartColorTemplates.material()
+            
+            let data = LineChartData(dataSet: set)
+            
+            lineChart.data = data
+          
+      }
     
 
     /*
--- application/ios-blue-tool-box/View/BlueTViewController.swift
+++ application/ios-blue-tool-box/View/BlueTViewController.swift
@@ -7,59 +7,214 @@
 //
 
 import UIKit
-import Charts
-class BlueTViewController: UIViewController,ChartViewDelegate {
+import CoreBluetooth
+import SwiftUI
 
-    var lineChart = LineChartView()
-      
-      
-      
-      
-      
-      
-      
-      
-      override func viewDidLoad() {
-          super.viewDidLoad()
-          view.backgroundColor = .cyan
-          lineChart.delegate = self
-          
-      }
-      
-      override func viewDidLayoutSubviews() {
-          super.viewDidLayoutSubviews()
-          
-          
-            lineChart.frame = CGRect(x: 0, y: 0,
-                                       width: self.view.frame.size.width, height:self.view.frame.size.width)
-            lineChart.center = view.center
-            view.addSubview(lineChart)
-            
-            var entries = [ChartDataEntry]()
-            
-            for x in 0..<10
-            {
-                entries.append(ChartDataEntry(x: Double(x),y: Double(x)))
-            }
-            
-            let set = LineChartDataSet(entries:entries)
-            set.colors = ChartColorTemplates.material()
-            
-            let data = LineChartData(dataSet: set)
-            
-            lineChart.data = data
-          
-      }
+
+class BlueTViewController:  UIViewController, UITableViewDataSource{
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private let refreshControl = UIRefreshControl()
+    
+    var isStartButton : Bool = true
+    var dictionnaryDoublon : [String: Sensor] = [:]
+    
+    class DisplayObject {
+        
+        public var name: String
+        public var RSSI: Int
+        public var identifier : String
+        public var displaytext : String
+        
+        init(name : String, RSSI: Int, identifier : String, displaytext : String)
+        {
+            self.name = name
+            self.RSSI = RSSI
+            self.identifier = identifier
+            self.displaytext = displaytext
+        }
     }
-    */
-
+    
+    var display: [ DisplayObject] = []
+    var displayalldata: [ DisplayObject ] = []
+    
+    private var scanner: Scanner1!
+    
+    func setDisplay(nom : String) -> [ DisplayObject]
+    {
+        for key in displayalldata
+        {
+            if(key.name == "MAG")
+            {
+                key.name = "toto"
+            }
+        }
+        return displayalldata
+    }
+    
+    func showToast(message: String, font: UIFont) {
+        let toastLabel = UILabel()
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = .white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+        
+        let maxWidthPercentage: CGFloat = 0.8
+        let maxTitleSize = CGSize(width: view.bounds.size.width * maxWidthPercentage, height: view.bounds.size.height * maxWidthPercentage)
+        var titleSize = toastLabel.sizeThatFits(maxTitleSize)
+        titleSize.width += 20
+        titleSize.height += 10
+        toastLabel.frame = CGRect(x: view.frame.size.width / 2 - titleSize.width / 2, y: view.frame.size.height - 50, width: titleSize.width, height: titleSize.height)
+        
+        view.addSubview(toastLabel)
+        
+        UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: { _ in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return display.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let objectDiplay = display[indexPath.row]
+        cell.textLabel?.text = objectDiplay.name + String(objectDiplay.RSSI)
+        return cell
+    }
+    
+    private let tableview : UITableView =
+    {
+        let tableview = UITableView()
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableview
+        
+    }()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        scanner = Scanner1()
+        scanner.initializeScanner()
+        tableview.contentInset = UIEdgeInsets(top: 60,left: 0,bottom: 0,right: 0)
+        tableview.backgroundColor = .green
+        tableview.dataSource = self
+        view.addSubview(tableview)
+        
+        tableview.reloadData()
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 50, width: 60, height: 60))
+        button.setImage(UIImage(systemName: "play"), for: .normal)
+        button.addTarget(self, action: #selector(self.buttonTapped), for: .touchUpInside)
+        view.addSubview(button)
+        
+        let buttonStop =  UIButton(frame: CGRect(x: 180, y: 50, width: 60, height: 60))
+        
+        buttonStop.setImage(UIImage(systemName: "stop"), for: .normal)
+        buttonStop.addTarget(self, action: #selector(self.buttonTappedStop), for: .touchUpInside)
+        view.addSubview(buttonStop)
+    }
+    
+    @objc func buttonTapped(sender : UIButton) {
+        
+        if (isStartButton == true)
+        {
+            scanner = Scanner1()
+            scanner.initializeScanner()
+            
+            scanner.dictionnarySensor.addHandler(handler : handleNewObjectAvailable)
+            print("El toro de Mardid")
+            
+            self.showToast(message: "Le scan a démarré", font: .systemFont(ofSize: 12.0))
+            
+            isStartButton = false
+        }
+    }
+    
+    @objc func buttonTappedStop(sender : UIButton) {
+        
+        scanner.stopScanner()
+        
+        print("El toro de Mardid")
+        self.showToast(message: "Le scan est terminé", font: .systemFont(ofSize: 12.0))
+        
+        self.display.removeAll()
+        tableview.reloadData()
+        isStartButton = true
+    }
+    
+    override func  viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableview.frame = view.bounds
+    }
+    
+    func handleNewObjectAvailable(data: ([String : Sensor])) {
+        
+        getSensorTemperature(data: data)
+        
+    }
+    
+    func getSensorTemperature(data: ([String : Sensor])) -> DisplayObject {
+        
+        //setDisplay(nom: "toto")
+        
+        var trouve = true
+        //
+        var newobject : DisplayObject = DisplayObject(name: "null", RSSI: 0, identifier: "", displaytext: "")
+        for (key,value) in data
+        {
+            if(value is SensorTemperature)
+            {
+                if let magnetic = value as? SensorTemperature
+                {
+                    //
+                    //display.removeAll()
+                    for var cle in self.display
+                    {
+                        print("indetifier cle" + cle.identifier + "identifier data " + key + cle.name)
+                        if(key == cle.identifier)
+                        {
+                            cle.RSSI = value.RSSI
+                            print("cle RSSSSSi" + String(cle.RSSI))
+                            //
+                            cle = DisplayObject(name : cle.name, RSSI : cle.RSSI, identifier:  cle.identifier, displaytext: cle.name + " " + String(cle.RSSI))
+                            print("updated !!!!!!!!!!!!!")
+                            trouve  = false
+                        }
+                    }
+                    //
+                    if(trouve == true)
+                    {
+                        newobject = DisplayObject(name: magnetic.name, RSSI: magnetic.RSSI, identifier:  magnetic.idenfitfier, displaytext: "")
+                        display.append(newobject)
+                    }
+                    //
+                    /*
+                    for item in displayalldata
+                    {
+                        display.append(item)
+                    }
+ */
+                    self.tableview.reloadData()
+                }
+            }
+        }
+        return newobject
+    }
 }
+
+
+
+
+
+
+
