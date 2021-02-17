ac0557ec7df22845bb004b89b1ff228a91fae87f
ELA Innovation <be-dev@Mac-mini-de-ELA.local>
2021-02-17 17:58:02 +0100

    ajout graphe debug

--- application/ios-blue-tool-box/Target_list/targetTempViewController.swift
+++ application/ios-blue-tool-box/Target_list/targetTempViewController.swift
@@ -3,6 +3,37 @@ import Charts
 
 // MARK: Extension
 
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
+
+
+
 extension UIButton {
     func blink() {
         self.alpha = 0.0;
@@ -40,7 +71,8 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
     private var entries = [ChartDataEntry]()
     private var entries1 = [ChartDataEntry]()
     private var lineChart = LineChartView()
-    
+    private var numberTemperature = 25
+   var UItemperature = UITextView()
     
     
     // Compteur graphe entries
@@ -72,6 +104,8 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
         super.init(nibName : nil , bundle : nil)
     }
     
+
+    
     
     @objc func imageTapTemp() {
         self.navigationController?.pushViewController(ConnectViewController(nameSensor: nameSensor, RSSI: RSSI, identifier: identifier, battery: battery, typedata:typedata, array: array, displayObject: displayObject), animated: true)
@@ -101,11 +135,14 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
                 {
                     if let tempHum = value as? SensorTemperature
                     {
+                        UItemperature.text = String(tempHum.getTemp()) + "°C"
                         entries.append(ChartDataEntry(x: Double(compteur),y: Double(tempHum.getTemp())))
                         compteur = compteur + 1
                         compHum = compHum + 1
                         
                         let set = LineChartDataSet(entries: entries, label: "temperature")
+                        set.setColor(UIColor(hexString: "#336699"))
+                        set.setCircleColor(UIColor(hexString: "#336699"))
                         let data = LineChartData(dataSet: set)
                         lineChart.data = data
                        
@@ -114,12 +151,30 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
                 }
             }
         }
+        if(entries.count >= 8)
+        {
+            entries.removeFirst(1)
+        }
+        
+        
         return newobject
     }
     
     
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
+        
+        let line = UIView(frame: CGRect(x: 40, y: 150, width: 290, height: 3))
+        line.backgroundColor =  UIColor(hexString: "#336699")
+        self.view.addSubview(line)
+        
+        let lineBack = UIView(frame: CGRect(x: 40, y: 200, width: 290, height: 3))
+        lineBack.backgroundColor = UIColor(hexString: "#336699")
+        self.view.addSubview(lineBack)
+        
         view.backgroundColor = .white
         scanner = Scanner1()
         scanner.initializeScanner()
@@ -132,10 +187,28 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
         UInameSensor.font = UIFont.systemFont(ofSize: 25.0)
         UInameSensor.isUserInteractionEnabled = false
         UInameSensor.font = UIFont.boldSystemFont(ofSize: 25)
-        UInameSensor.frame = CGRect(x: 80, y: 50, width: 350, height: 100)
+        UInameSensor.frame = CGRect(x: 80, y: 70, width: 350, height: 100)
         UInameSensor.backgroundColor = .none
         self.view.addSubview(UInameSensor)
         self.view.bringSubviewToFront(UInameSensor)
+        
+        
+        
+        UItemperature.text = String("")
+        UItemperature.textColor = UIColor.black
+        UItemperature.font = UIFont.systemFont(ofSize: 25.0)
+        UItemperature.isUserInteractionEnabled = false
+        UItemperature.font = UIFont.boldSystemFont(ofSize: 25)
+        UItemperature.frame = CGRect(x: 150, y: 150, width: 350, height: 100)
+        UItemperature.backgroundColor = .none
+        self.view.addSubview(UItemperature)
+        self.view.bringSubviewToFront(UItemperature)
+        
+
+        
+        
+        
+
         
         
         if(battery != -1)
@@ -188,7 +261,7 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
         super.viewDidLayoutSubviews()
         
         
-        lineChart.frame = CGRect(x: 0, y: 0,width: self.view.frame.size.width, height:self.view.frame.size.width)
+        lineChart.frame = CGRect(x: 0, y: 0,width: 350, height:200)
         lineChart.center = view.center
         
         view.addSubview(lineChart)
@@ -200,6 +273,7 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
             {
                 if let temp : [Capteur.Temp] = cle.array as? [Capteur.Temp]
                 {
+                  
                     for x in 0..<cle.array.count
                     {
                         entries.append(ChartDataEntry(x: Double(x),y: Double(temp[x].getTemp())))
@@ -210,6 +284,8 @@ class targetTempViewController: UIViewController,ChartViewDelegate {
             }
         }
         let set = LineChartDataSet(entries: entries, label: "temperature")
+        set.setColor(UIColor(hexString: "#336699"))
+        set.setCircleColor(UIColor(hexString: "#336699"))
         let data = LineChartData(dataSet: set)
         lineChart.data = data
         
